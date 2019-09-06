53fdecd7f3c67449839154664b3192064a8c1b21
DamianCameron <damiancameron99@gmail.com>
2019-09-06 15:40:46 +0200

    * Added h03, wrote assignment 4.

---
+++ src/h03/Opdracht4.java
@@ -0,0 +1,43 @@
+package h03;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht4 extends Applet {
+
+    String blauw;
+    String rood;
+    String geel;
+
+    public void init() {
+        setSize(400,400);
+        setBackground(Color.pink);
+
+        blauw = "Jeroen";
+        rood = "Hans";
+        geel = "Valerie";
+
+            }
+
+    public void paint(Graphics g) {
+
+        //statistieken
+        g.setColor(Color.GRAY);
+        g.fillRect(0,360,400,40);//UI
+
+        g.setColor(Color.BLUE);
+        g.fillRect(50,160,50,200);//Jeroen
+
+        g.setColor(Color.red);
+        g.fillRect(150,200,50,160);//Hans
+
+        g.setColor(Color.YELLOW);
+        g.fillRect(250,280,50,80);//Valerie
+
+        //Namen
+        g.setColor(Color.green);
+        g.drawString(blauw,50,380);
+        g.drawString(rood,150,380);
+        g.drawString(geel,250,380);
+            }
+}
