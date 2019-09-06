eb29b624100403e7643ba53a350173a32b4547f7
DamianCameron <damiancameron99@gmail.com>
2019-09-06 15:19:22 +0200

    * Added h03, wrote assignment 3.

---
+++ src/h03/Opdracht3.java
@@ -0,0 +1,29 @@
+package h03;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht3 extends Applet {
+
+    public void init() {
+        setSize(400,400);
+        setBackground(Color.pink);
+            }
+
+    public void paint(Graphics g) {
+
+
+        //vlag
+        g.setColor(Color.red);
+        g.fillRect(60,50,200,70); //rood
+
+        g.setColor(Color.WHITE);
+        g.fillRect(60,120,200,70); //wit
+
+        g.setColor(Color.BLUE);
+        g.fillRect(60,190,200,70); //blauw
+
+        g.setColor(Color.GRAY);
+        g.fillRect(30,50,30,350); //paal
+            }
+}
