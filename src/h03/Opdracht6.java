03b1cadc18f6136f353534978f3f1051a1d08532
DamianCameron <damiancameron99@gmail.com>
2019-09-06 16:08:26 +0200

    * Added h03, wrote assignment 6.

---
+++ src/h03/Opdracht6.java
@@ -0,0 +1,30 @@
+package h03;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht6 extends Applet {
+
+    public void init() {
+        setSize(400,400);
+        setBackground(Color.BLUE);
+            }
+
+    public void paint(Graphics g) {
+
+        //pole
+        g.setColor(Color.DARK_GRAY);
+        g.fillRect(150,100,100,280);
+        g.fillRect(175,380,50,20);
+
+        //lights
+        g.setColor(Color.red);
+        g.fillOval(175,125,50,50);
+
+        g.setColor(Color.ORANGE);
+        g.fillOval(175,200,50,50);
+
+        g.setColor(Color.green);
+        g.fillOval(175,275,50,50);
+            }
+}
