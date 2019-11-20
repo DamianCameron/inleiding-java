2f566b4b722d9cda3530075b3549fd0a424e9141
DamianCameron <damiancameron99@gmail.com>
2019-11-20 12:42:45 +0100

    * h011 Made Opdracht1

--- src/h011/Opdracht1.java
+++ src/h011/Opdracht1.java
@@ -1,4 +1,20 @@
 package h011;
 
-public class Opdracht1 {
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht1 extends Applet {
+
+    public void init() {}
+
+    public void paint(Graphics g) {
+        int teller = 0;
+        int y = 0;
+
+        while(teller < 10) {
+            y += 20;
+            g.drawLine(50, y, 300, y );
+            teller++;
+        }
+    }
 }
